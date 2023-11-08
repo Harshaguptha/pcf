290f139710fe05cd960f73c89a3ff58c8d26d8dc
harshaguptha <harsha.devops9998@gmail.com>
2023-11-08 12:22:26 +0530

    Updated

--- sample.sh
+++ sample.sh
@@ -4,9 +4,11 @@ nonprod
 nonprod-east
 nonprod-west
 )
-ORIGINALBRANCH=`git status | head -n1 | cut -c13-`
+#ORIGINALBRANCH=`git status | head -n1 | cut -c13-`
+ORIGINALBRANCH=`git status`
 git commit -m $1
-CHERRYCOMMIT=`git log -n1 | head -n1 | cut -c8-`
+#CHERRYCOMMIT=`git log -n1 | head -n1 | cut -c8-`
+CHERRYCOMMIT=`git log`
 for BRANCH in "${BRANCHES[@]}";
 do
     git stash;
